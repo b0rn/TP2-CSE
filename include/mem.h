faaabdbddeb3efcec8e87783b66f03d2686e70d3
Victor Leveneur <victor.leveneur@gmail.com>
2019-09-27 14:35:51 +0200

    Différentes stratégies implémentées

--- include/mem.h
+++ include/mem.h
@@ -1,6 +1,10 @@
 #if !defined(__MEM_H)
 #define __MEM_H
+#include <stdio.h>
 #include <stddef.h>
+#include "mem_os.h"
+#include "common.h"
+#include "fb.h"
 
 /* -----------------------------------------------*/
 /* Interface utilisateur de votre allocateur      */
@@ -12,4 +16,8 @@ size_t mem_get_size(void *);
 
 /* Itérateur sur le contenu de l'allocateur */
 void mem_show(void (*print)(void *, size_t, int free));
+void mem_fit(mem_fit_function_t* mff);
+struct fb* mem_first_fit(struct fb* head, size_t size);
+struct fb* mem_best_fit(struct fb* head, size_t size);
+struct fb* mem_worst_fit(struct fb* head, size_t size);
 #endif
--- src/mem.c
+++ src/mem.c
@@ -1,29 +1,23 @@
-
 #include "mem.h"
-#include "mem_os.h"
-#include "common.h"
-#include "fb.h"
-#include <stdio.h>
-
 //-------------------------------------------------------------
 // mem_init
 //-------------------------------------------------------------
 void mem_init() {
-    mem_fit(&mem_first_fit);
+  mem_fit(&mem_first_fit);
 
-    fb **ffb = (fb**)((mem_fit_function_t **) get_memory_adr() + sizeof(mem_first_fit*));
-    *fbb = ffb + sizeof(fb*);
-    fb *fb = *fbb;
-    fb->size = MEMORY_SIZE - sizeof(mem_fit_function_t*) - sizeof(fb*);
-    fb->next = NULL;
+  fb **ffb = (fb**)((mem_fit_function_t **) get_memory_adr() + sizeof(mem_fit_function_t*));
+  *ffb = (fb*)(ffb + sizeof(fb*));
+  fb *fb = *ffb;
+  fb->size = MEMORY_SIZE - sizeof(mem_fit_function_t *);
+  fb->next = NULL;
 }
 
 //-------------------------------------------------------------
 // mem_alloc
 //-------------------------------------------------------------
 void* mem_alloc(size_t size) {
-   /* A COMPLETER */
-        return NULL;
+  /* A COMPLETER */
+  return NULL;
 }
 
 //-------------------------------------------------------------
@@ -45,26 +39,35 @@ void mem_show(void (*print)(void *, size_t, int free)) {
 // mem_fit
 //-------------------------------------------------------------
 void mem_fit(mem_fit_function_t* mff) {
-    mem_fit_function_t** fit_function = (mem_fit_function_t **) get_memory_adr();
-    *fit_function = mff;
+  mem_fit_function_t** fit_function = (mem_fit_function_t **) get_memory_adr();
+  *fit_function = mff;
 }
 
 //-------------------------------------------------------------
 // Stratégies d'allocation
 //-------------------------------------------------------------
 struct fb* mem_first_fit(struct fb* head, size_t size) {
-    fb **ffb = (fb**)((mem_fit_function_t **) get_memory_adr() + sizeof(mem_first_fit*));
-
-    return NULL;
+  fb *b = head;
+  while(b->size < size && b->next != NULL) b = b->next;
+  return (b->size >= size)?b:NULL;
 }
 //-------------------------------------------------------------
 struct fb* mem_best_fit(struct fb* head, size_t size) {
-   /* A COMPLETER */
-    return NULL;
-
+  fb *best = NULL, *b = head;
+  while(b->next != NULL){
+    if(b->size >= size)
+      best = (best == NULL)?b:((b->size < best->size)?b:best);
+    b = b->next;
+  }
+  return best;
 }
 //-------------------------------------------------------------
 struct fb* mem_worst_fit(struct fb* head, size_t size) {
-   /* A COMPLETER */
-    return NULL;
+  fb *best = NULL, *b = head;
+  while(b->next != NULL){
+    if(b->size >= size)
+      best = (best == NULL)?b:((b->size > best->size)?b:best);
+    b = b->next;
+  }
+  return best;
 }
